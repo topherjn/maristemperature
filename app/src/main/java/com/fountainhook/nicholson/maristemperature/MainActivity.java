4875b79034fcced7c0c2dc6f5a19b111024aba1b
Christopher Nicholson <nicholsonc@gmail.com>
2015-12-09 23:30:29 -0500

    Initial commit with temperature displaying in TextView and updating.

---
+++ app/src/main/java/com/fountainhook/nicholson/maristemperature/MainActivity.java
@@ -0,0 +1,77 @@
+package com.fountainhook.nicholson.maristemperature;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class MainActivity extends ActionBarActivity {
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private TextView tempVal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tempVal = (TextView) findViewById(R.id.tempVal);
+
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_TEMPERATURE);
+        mSensorManager.registerListener(temperatureListener, mSensor, SensorManager.SENSOR_DELAY_FASTEST);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private double celsiusToFahren(double celsius)
+    {
+        return 9.0 / 5.0 * celsius + 32;
+    }
+    private final SensorEventListener temperatureListener = new SensorEventListener(){
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            double temperature = event.values[0];
+
+            tempVal.setText("Temperature is: "+ celsiusToFahren(temperature));
+
+        }
+    };
+
+}
